616259e3d1c8680b8adbcb358c6aade0adc5c8fd
Zhang Jie <zhangjie.iet@gmail.com>
2016-05-14 23:24:07 +0800

    update interpret

--- nlp/interpreter.py
+++ nlp/interpreter.py
@@ -1,11 +1,30 @@
 import re
 
-from common import COMMANDS,States
+from common import Commands,States
 
+def is_create_item(user,msg_obj):
+    if (user.last_state == States.STATIC):
+        try:
+            return msg_obj['attachments']['type']=='image'
+        except:
+            return False
+    return False
 
-MSG_TO_COMMAND = {x:x for x in COMMANDS}
+def is_edit_desc(user,msg_obj):
+    if (user.last_state == States.EDIT or user.last_state == States.ADD_NEW):
+        try:
+            return len(msg_obj["text"])>0
+        except:
+            return False
+    return False
 
-
+def is_edit_pic(user,msg_obj):
+    if (user.last_state == States.EDIT or user.last_state == States.ADD_NEW):
+        try:
+            return msg_obj['attachments']['type']=='image'
+        except:
+            return False
+    return False
 
 def interpret(message,*args,**kwargs):
     """
@@ -13,5 +32,14 @@ def interpret(message,*args,**kwargs):
     command is string, command_kwargs is a dict
     """
     user = kwargs.get("user")
+    msg_obj = kwargs.get("msg_obj")
     lowered_msg = ''.join(re.findall('[a-z]',message.lower()))
-    return MSG_TO_COMMAND.get(lowered_msg),{}
+    if (is_create_item(user,msg_obj)):
+        return Commands.CREATE_ITEM,{'img_url':msg_obj['attachments']}
+    elif(is_edit_desc(user,msg_obj)):
+        return Commands.EDIT_ITEM,{'text':msg_obj['text']}
+    elif(is_edit_pic(user,msg_obj)):
+        return Commands.EDIT_ITEM,{'img_url':msg_obj['attachments']}
+    elif(user.last_state == States.CHANGE_LOCATION):
+        return Commands.LOCATION_SAVED,{'text':msg_obj['text']}
+    return lowered_msg,{'msg_obj':msg_obj}
